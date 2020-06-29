438abe5580a8497020e0fbe4003636f6269602d2
RunTime256 <runtime256@gmail.com>
2020-06-28 19:33:59 -0500

    Fixed role mapper to have parameter names

--- src/main/java/bot/discord/role/RoleMapper.java
+++ src/main/java/bot/discord/role/RoleMapper.java
@@ -1,10 +1,11 @@
 package bot.discord.role;
 
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 public interface RoleMapper
 {
     @Select("SELECT role_id from manaphy.bot.role r JOIN manaphy.bot.guild g ON r.guild_id = g.guild_id WHERE " +
             "g.name = #{guildName} AND r.name = #{roleName}")
-    Long getRole(String guildName, String roleName);
+    Long getRole(@Param("guildName") String guildName, @Param("roleName") String roleName);
 }
