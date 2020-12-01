16c3b89fb177b93aaf3f7bb06bce01b5a01747fa
RunTime256 <runtime256@gmail.com>
2020-11-30 20:47:44 -0600

    Fixed scorecards with null values

--- src/main/java/war/scorecard/ScorecardMapper.java
+++ src/main/java/war/scorecard/ScorecardMapper.java
@@ -9,7 +9,7 @@ public interface ScorecardMapper
     @Select("WITH battle_tokens AS (SELECT 0 AS tokens), " +
             "puzzle_tokens AS (SELECT 0 AS tokens), " +
             "art_tokens AS (SELECT 0 AS tokens), " +
-            "game_tokens AS (SELECT SUM(gp.tokens) AS tokens FROM cc4.game_points gp WHERE gp.user_id = #{userId}), " +
+            "game_tokens AS (SELECT COALESCE(SUM(gp.tokens), 0) AS tokens FROM cc4.game_points gp WHERE gp.user_id = #{userId}), " +
             "misc_tokens AS (SELECT 0 AS tokens) " +
             "SELECT #{userId} AS user_id, bt.tokens AS battle_tokens, pt.tokens AS puzzle_tokens, at.tokens AS art_tokens, gt.tokens AS game_tokens, mt.tokens AS misc_tokens " +
             "FROM battle_tokens bt JOIN puzzle_tokens pt ON 1 = 1 JOIN art_tokens at ON 1 = 1 JOIN game_tokens gt ON 1 = 1 JOIN misc_tokens mt ON 1 = 1 ")
